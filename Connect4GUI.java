20bbe586c6e65076d000105873899da3f4d6a72a
John Wilkie <John Wilkie>
2016-01-26 22:07:34 +0400

    created a class for GUI and built a JFrame

---
+++ Connect4GUI.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Connect4GUI{
+
+    public Connect4GUI(){
+        String title  = "Connect 4 Game";
+        JFrame mainWindow = new JFrame(title);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setVisible(true);
+        
+    }
+
+}
